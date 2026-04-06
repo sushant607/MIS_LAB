9637963786ecf6df098e38d33a32c94ea0dac51c
jayashbhuyar <jayashbhuyar@gmail.com>
2026-04-06 17:30:00 +0000

    add base pages

---
+++ frontend/src/pages/Index.tsx
@@ -0,0 +1,9 @@
+// Update this page (the content is just a fallback if you fail to update the page)
+
+import { Navigate } from "react-router-dom";
+
+const Index = () => {
+  return <Navigate to="/dashboard" replace />;
+};
+
+export default Index;
